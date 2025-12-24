257686fab63c1f6ccaf2bd97009ee887ca47e7e8
Sann Siv <sannsiv49@gmail.com>
2025-12-24 11:23:59 +0700

    fix: Add feature show passowrd

--- JS/signup.js
+++ JS/signup.js
@@ -4,6 +4,25 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const confirmPasswordInput = document.getElementById('confirm_password');
 const form = document.getElementById('signup-form');
+const showPassword= document.getElementById('show-ps');
+
+
+showPassword.addEventListener('click',function(){
+    if (passwordInput.type === "password"){
+        passwordInput.type = "text";
+    }else{
+        passwordInput.type = "password";
+    }
+})
+showPassword.addEventListener('click',function(){
+    if (confirmPasswordInput.type === "password"){
+        confirmPasswordInput.type = "text";
+    }else{
+        confirmPasswordInput.type = "password";
+    }
+})
+
+
 
 
 const users = JSON.parse(localStorage.getItem("users")) || [];
@@ -16,20 +35,35 @@ form.addEventListener('submit', function (e) {
     const password = passwordInput.value;
     const confirmPassword = confirmPasswordInput.value;
 
-    if (!username || !email || !password || !confirmPassword) {
-        alert("Please fill in all the information.");
+    
+
+    if (!username && !email && !password && !confirmPassword ){
+        alert("Please fill in all the information.❗");
         return;
     }
-
-    if (password.length < 8) {
+    
+    else if (!username){
+        alert("please fill in your username. 🏷️")
+        return;
+    }else if(!email){
+        alert("please fill in your email. 📩")
+        return;
+    }else if (!password){
+        alert("please fill in your password. 🔑")
+    }else if (!confirmPassword){
+        alert("please fill in your confirm password. ✍️")
+    }
+    
+    else if (password.length < 8) {
         alert("Password must be at least 8 characters.");
         return;
     }
-
-    if (password !== confirmPassword) {
+    else if (password !== confirmPassword) {
         alert("Password and Confirm Password do not match!");
         return;
     }
+
+
 
     const userData = {
         username,
@@ -48,7 +82,7 @@ form.addEventListener('submit', function (e) {
 
 passwordInput.addEventListener('input', function () {
     if (passwordInput.value.length >= 8) {
-        textValidation.style.color = 'green';
+        textValidation.style.color = '#47f20fff';
     } else {
         textValidation.style.color = 'red';
     }
